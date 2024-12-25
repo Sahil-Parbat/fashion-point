0442b0319e3b9015a960f90a77ae278beaa2a97e
Sahil-Parbat <sahilparbat24@gmail.com>
2024-12-25 18:22:08 +0530

    adding Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
+import Header from "./Header/Header";
 import "./App.css";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+    <Header/>
     </div>
   );
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+const Header =()=>{
+    return(<>
+        This Is Our New Project
+    </>)
+    
+}
+export default Header;
